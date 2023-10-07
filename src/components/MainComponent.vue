d2756a4e1f90b278f685aa1dfaa65fe651b39f39
K-a-R-a-T-e-L-L <kirillcuhorukov6@gmail.com>
2023-10-07 23:09:09 +0700

    Тут был создан легендарный красный квадрат 40х40 px

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div :style="RedBox"></div>
+  <img src=""/>
+</template>
+
+<script>
+export default {
+  name: 'MainComponent',
+  computed: {
+    RedBox(){
+      return{
+        width: '40px',
+        height: '40px',
+        backgroundColor: 'red'
+      }
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
